6fbf86d9e4e4a34eaa3ee7239f8cbbbb2a94331d
Huynh Thanh Quy <quyht0251@ut.edu.vn>
2025-12-20 10:02:26 +0700

    Deleted tic-tac-toe and add UI HomeView for src/TicTacToe.Client

---
+++ src/TicTacToe.Client/Views/HomeView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace TicTacToe.Client.Views
+{
+    // Quan trọng: Phải kế thừa từ UserControl nếu XAML của bạn là <UserControl>
+    public partial class HomeView : UserControl
+    {
+        public HomeView()
+        {
+            InitializeComponent(); // Lệnh này dùng để nạp giao diện từ XAML lên
+        }
+    }
+}
