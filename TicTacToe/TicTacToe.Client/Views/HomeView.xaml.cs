7059c095a2fb2e79edd003cc75aa01c5b3ff92db
huuphuoc <huuphuoc.md@gmail.com>
2026-01-04 23:18:21 +0700

    Thay đổi UI

--- TicTacToe/TicTacToe.Client/Views/HomeView.xaml.cs
+++ TicTacToe/TicTacToe.Client/Views/HomeView.xaml.cs
@@ -6,20 +6,23 @@ namespace TicTacToe.Client.Views;
 
 public partial class HomeView : UserControl
 {
+    // Sự kiện để MainWindow có thể lắng nghe và chuyển màn hình
     public event Action? PlayRequested;
 
     public HomeView()
     {
-        InitializeComponent(); // chỉ gọi, không tự định nghĩa
+        InitializeComponent();
     }
 
     private void Play_Click(object sender, RoutedEventArgs e)
     {
+        // Khi nhấn nút, gọi sự kiện để báo cho lớp cha (MainWindow)
         PlayRequested?.Invoke();
     }
 
     private void Exit_Click(object sender, RoutedEventArgs e)
     {
+        // Đóng ứng dụng hoàn toàn
         Application.Current.Shutdown();
     }
 }
