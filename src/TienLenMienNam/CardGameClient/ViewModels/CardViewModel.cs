178c19a19dad78070ce1a121281cf7e9093a3f83
hongnhan1 <hongnhan210127@gmail.com>
2026-01-08 17:19:28 +0700

    sửa kết nối server

---
+++ src/TienLenMienNam/CardGameClient/ViewModels/CardViewModel.cs
@@ -0,0 +1,34 @@
+﻿// CardGameClient/ViewModels/CardViewModel.cs
+using System.Windows.Media;
+using CardGameClient.Models;
+
+namespace CardGameClient.ViewModels
+{
+    internal class CardViewModel : ViewModelBase
+    {
+        public Card Card { get; }
+
+        private bool _isSelected;
+
+        public CardViewModel(Card card)
+        {
+            Card = card;
+        }
+
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                if (_isSelected == value) return;
+                _isSelected = value;
+                Raise();
+                Raise(nameof(BorderBrush));
+            }
+        }
+
+        public Brush BorderBrush => IsSelected ? Brushes.Gold : Brushes.Transparent;
+
+        public string Code => Card.ToCode();
+    }
+}
