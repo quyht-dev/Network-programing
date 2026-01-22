e3631dbd78d4d04ef836490faa0e26ab9cddaa06
tuyen84@@ <tuyenhv1256@ut.edu.vn>
2026-01-22 20:52:08 +0700

    Cập nhật thêm  giao diện đổi nút và X O trò chơi tic tac toe

--- src/TicTacToe/TicTacToe.Client/Views/GameView.xaml.cs
+++ src/TicTacToe/TicTacToe.Client/Views/GameView.xaml.cs
@@ -1,13 +1,28 @@
+using System.Windows;
 using System.Windows.Controls;
 using TicTacToe.Client.ViewModels;
 
-namespace TicTacToe.Client.Views;
-
-public partial class GameView : UserControl
+namespace TicTacToe.Client.Views
 {
-    public GameView()
+    public partial class GameView : UserControl
     {
-        InitializeComponent();
-        DataContext = new MainViewModel();
+        public GameView()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+
+        private void UserControl_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // Nếu chiều rộng nhỏ hơn 650px thì chuyển sang giao diện dọc
+            if (e.NewSize.Width < 650)
+            {
+                VisualStateManager.GoToState(this, "NarrowState", true);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, "WideState", true);
+            }
+        }
     }
 }
